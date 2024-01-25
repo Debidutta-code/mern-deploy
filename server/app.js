c33fb3c0beb04ff8058c74d720055b074baff4cf
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-01-25 21:37:04 +0530

    initial commit :tada

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+
+
+
+// middleware
+
+// to make it more secure to not access from any port except than 3000
+const corsOption = {
+    origin : "http://localhost:3000"
+}
+
+app.use(express.json());
+app.use(cors(corsOption));
+
+// connect mongodb
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("db connected..");
+}
+
+
+
+
+// requests
+
+app.get('/', (req, res) => {
+    res.json({message : "Hello world"});
+})
+
+
+
+
+
+// backend connection with listen
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+})
