914140860766219bfd29134bd3ab57892a54d3c3
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-01-25 23:05:01 +0530

    this is dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState("");
-  
-  const handleClick = async () => {
+
+  useEffect( async () => {
     const response = await fetch("http://localhost:4000/home", {
       method: "GET"
-      
+
     })
     const data = await response.json();
     if(data.success){
       setMessage(data.message);
     }
-  }
+  }, [])
 
   return (
     <div className="App">
-      <div>{message}</div>
-      <button onClick={handleClick}>BUTTON</button>
+      <h1>{message}</h1>
     </div>
   );
 }
