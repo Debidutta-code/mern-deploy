318bb0e935a7cd3f53e317a2c1ba2298e2e5b086
DEBIDUTTA ACHARYA <77004029+Debidutta-code@users.noreply.github.com>
2024-01-26 11:24:11 +0530

    add loading message

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,36 @@ import { useState } from 'react';
 
 function App() {
   const [message, setMessage] = useState("hello");
-  
+  const [loading, setLoading] = useState(false);
+
   const handleClick = async () => {
-    const response = await fetch("https://mern-demo-ojek.onrender.com/", {
-      method: "GET"
-    })
-    const data = await response.json();
-    if(data.success){
-      setMessage(data.message);
+    setLoading(true);
+
+    try {
+      const response = await fetch("https://mern-demo-ojek.onrender.com/", {
+        method: "GET"
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setMessage(data.message);
+      } else {
+        setMessage("Error fetching data from the backend");
+      }
+    } catch (error) {
+      setMessage("An error occurred while fetching data");
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className="App">
-      <h1>{message}</h1>
-      <button className='btn' onClick={handleClick}>BUTTON</button>
+      <h1>{loading ? "Loading..." : message}</h1>
+      <button className='btn' onClick={handleClick} disabled={loading}>
+        BUTTON
+      </button>
     </div>
   );
 }
